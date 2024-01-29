02e1ee0aee54f103e492561e9064b13972223285
Ridwan778 <113521598+Ridwan778@users.noreply.github.com>
2024-01-29 19:54:02 -0330

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import {getAuth, GoogleAuthProvider} from "firebase/auth";
 import {getFirestore} from 'firebase/firestore'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyB_fUGxSRaSQVAlJX8pI8sOwMGvU47kc4k",
   authDomain: "chitchat-e8181.firebaseapp.com",
@@ -15,7 +12,6 @@ const firebaseConfig = {
   appId: "1:606122015484:web:0f9ac8cbc77ebd0dfd3579"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
